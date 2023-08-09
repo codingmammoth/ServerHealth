b2fd08ec5ec1655f962d272f994c4204461a4fa2
J.T. Kalkman <mail@jtkalkman.nl>
2023-08-09 11:13:31 +0100

    Adds method to determine the highest state to ServerStates.

--- src/ServerHealth/ServerStates.php
+++ src/ServerHealth/ServerStates.php
@@ -4,4 +4,17 @@ class ServerStates {
     final public const ok = 'ok';
     final public const warning = 'warning';
     final public const error = 'error';
+
+    public static function getHighestState(array $states): bool|string
+    {
+        if (in_array(ServerStates::error, $states)) {
+            return ServerStates::error;
+        } else if (in_array(ServerStates::warning, $states)) {
+            return ServerStates::warning;
+        } else if (in_array(ServerStates::ok, $states)) {
+            return ServerStates::ok;
+        } else {
+            return false;
+        }
+    }
 }
