dc82f95892eae0b488d8716a46ace5d6a0517e96
J.T. Kalkman <mail@jtkalkman.nl>
2023-08-09 16:22:56 +0100

    Creates sql ping test.

--- src/ServerHealth/tests/MySQLPing.php
+++ src/ServerHealth/tests/MySQLPing.php
@@ -1,28 +1,40 @@
 <?php
 
-require_once __DIR__."../../ServerHealthTest.php";
-require_once __DIR__."../../functions/getStartTime.php";
-require_once __DIR__."../../functions/getRunningTime.php";
+require_once __DIR__ . "/MySQLTest.php";
 
-class MySQLPing extends ServerHealthTest {
+class MySQLPing extends MySQLTest
+{
+    protected string $name = 'MySQL Ping';
 
-	protected string $name = 'MySQL Ping';
+    public function run($db)
+    {
+        set_time_limit(0);
+        $starttime = getStartTime();
 
-	protected function performTest(): void
-	{
-		set_time_limit(0);
-		$starttime = getStartTime();
+        try {
+            if (mysqli_ping($db)) {
+                $totaltime = getRunningTime($starttime);
 
-		$conn = mysqli_connect($this->config['host'], $this->config['dbuser'], $this->config['dbpass'], $this->config['database'], $this->config['dbport']);
-		if (!mysqli_ping($conn)) {
-			$this->status = ServerStates::error;
-			$this->description = "Failed to ping, connect or login on the db-server";
-		} else {
-			$this->status = ServerStates::ok;
-			$totaltime = getRunningTime($starttime);
-			$this->value = $totaltime;
-			$this->description = "Total time: $totaltime";
-		}
-	}
-
+                $this->result = new ServerHealthResult(
+                    $this->name,
+                    ServerStates::ok,
+                    "Total time: $totaltime",
+                    $totaltime
+                );
+            } else {
+                $this->result = new ServerHealthResult(
+                    $this->name,
+                    ServerStates::error,
+                    "Failed to ping, connect or login on the db-server"
+                );
+            }
+        } catch (\Throwable $th) {
+            $error = $th->getMessage();
+            $this->result = new ServerHealthResult(
+                $this->name,
+                ServerStates::error,
+                "Failed to ping, connect or login on the db-server. Error: $error"
+            );
+        }
+    }
 }
