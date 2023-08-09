b60e1e1664c63ea48dbf52ff830ea17ec561ae72
J.T. Kalkman <mail@jtkalkman.nl>
2023-08-09 20:36:20 +0100

    Adds sample data in config file.

--- config/config.php
+++ config/config.php
@@ -2,9 +2,19 @@
 
 $error_level = E_ALL;
 
-$dbhost   = ''; // Database-hostname (default: localhost)
-$dbuser   = ''; // Database-username
-$dbpass   = ''; // Database-password
-$database = ''; // Database name (database to select)
-$dbport   = 3306; // Default port number
-$dbtable  = ''; // Database table (to perform a little `select` on)
+$db_host  = ''; // Database-hostname (default: localhost)
+$db_user  = 'sammy'; // Database-username
+$db_pass  = 'password'; // Database-password
+$db_name  = 'example_database'; // Database name (database to select)
+$db_port  = 3306; // Default port number
+$db_tables = ['todo_list']; // Database table (to perform a little `select` on)
+
+$db_config = [
+    'db_host'  => $db_host, 
+    'db_user'  => $db_user, 
+    'db_user'  => $db_user, 
+    'db_pass'  => $db_pass, 
+    'db_name'  => $db_name, 
+    'db_port'  => $db_port,
+    'db_tables' => $db_tables
+];
