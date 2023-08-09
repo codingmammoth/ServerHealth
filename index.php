9ce50e2df225205cdb04373f84d10c220187a4b8
J.T. Kalkman <mail@jtkalkman.nl>
2023-08-09 08:44:27 +0100

    Creates load tests for last five and last fifteen minutes.

--- index.php
+++ index.php
@@ -4,13 +4,17 @@ require_once __DIR__."/config/config.php";
 require_once __DIR__."/src/ServerHealth/ServerHealth.php";
 require_once __DIR__."/src/ServerHealth/tests/PHPLus.php";
 require_once __DIR__."/src/ServerHealth/tests/LoadNow.php";
+require_once __DIR__."/src/ServerHealth/tests/LoadAverageFiveMinutes.php";
+require_once __DIR__."/src/ServerHealth/tests/LoadAverageFifteenMinutes.php";
 
 error_reporting($error_level);
 
 $health = new ServerHealth();
 $health->tests([
     new PHPLus(['warning_threshold' => 0.5, 'error_threshold' => 1.0]),
-    new LoadNow(['warning_threshold' => 15, 'error_threshold' => 80])
+    new LoadNow(['warning_threshold' => 15, 'error_threshold' => 80]),
+    new LoadAverageFiveMinutes(['warning_threshold' => 15, 'error_threshold' => 80]),
+    new LoadAverageFifteenMinutes(['warning_threshold' => 15, 'error_threshold' => 80])
 ]);
 $health->run();
 $health->getResults();
---
+++ src/ServerHealth/tests/LoadAverageFifteenMinutes.php
@@ -0,0 +1,34 @@
+<?php
+
+require_once __DIR__."../../ServerHealthTest.php";
+require_once __DIR__."../../functions/getLoads.php";
+
+class LoadAverageFifteenMinutes extends ServerHealthTest {
+
+	protected string $name = 'Load average 5 minutes';
+
+	protected function performTest(): void
+	{
+		$load = getLoads(15);
+
+		$warning_threshold = isset($this->config['warning_threshold']) ? $this->config['warning_threshold'] : 15;
+		$error_threshold = isset($this->config['error_threshold']) ? $this->config['error_threshold'] : 80;
+
+		if ($load === false) {
+			$this->status = ServerStates::error;
+			$this->description = "Couldn't get load.";
+		} else if ($load > $error_threshold) {
+			$this->status = ServerStates::error;
+		} else if ($load > $warning_threshold) {
+			$this->status = ServerStates::warning;
+		} else {
+			$this->status = ServerStates::ok;
+		}
+		
+		if ($load !== false) {
+			$this->description = "Load avarage last fifteen minutes: $load";
+			$this->value = $load;
+		}
+	}
+
+}
