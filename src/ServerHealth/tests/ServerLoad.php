0b9f0210e3f5ff4c3e0c5917475a2aaf7c921678
J.T. Kalkman <mail@jtkalkman.nl>
2023-08-11 12:57:48 +0100

    Updates server load test.

--- src/ServerHealth/tests/ServerLoad.php
+++ src/ServerHealth/tests/ServerLoad.php
@@ -1,12 +1,13 @@
 <?php
 
-require_once __DIR__."../../ServerHealthTest.php";
+require_once __DIR__ . "../../ServerHealthTest.php";
 
-class ServerLoad extends ServerHealthTest {
+class ServerLoad extends ServerHealthTest
+{
 
-	protected string $name = 'Server Load';
+    protected $name = 'Server Load';
 
-    protected function getLoads(): array
+    protected function getLoads()
     {
         $loads = sys_getloadavg();
 
@@ -17,62 +18,57 @@ class ServerLoad extends ServerHealthTest {
         }
     }
 
-	protected function performTests(): void
-	{   
+    protected function performTests()
+    {
         $loads = $this->getLoads();
 
-		$warning_threshold = isset($this->config['warning_threshold']) ? $this->config['warning_threshold'] : 35;
-		$error_threshold = isset($this->config['error_threshold']) ? $this->config['error_threshold'] : 80;
-
         if ($loads === false) {
-            $result = new ServerHealthResult($this->name, ServerStates::error, "Couldn't get loads of server");
-            $this->results[] = $result;
-        } else if (!isset($this->config['tests']) || count($this->config['tests']) === 0) {
-            $result = new ServerHealthResult($this->name, ServerStates::error, "No loads to check.");
-            $this->results[] = $result;
-		} else {
-            foreach ($this->config['tests'] as $selected_load) {
+            $result = new ServerHealthResult($this->name, 2, "Couldn't get loads of server");
+            $this->result = $result;
+        } else if (!isset($this->config['type'])) {
+            $result = new ServerHealthResult($this->name, 2, "No load to check.");
+            $this->result = $result;
+        } else {
+            $warning_threshold = isset($this->config['warning_threshold']) ? $this->config['warning_threshold'] : 50;
+            $error_threshold = isset($this->config['error_threshold']) ? $this->config['error_threshold'] : 75;
+
+            $name = '';
+            $load = false;
+            $description = '';
+            $status = false;
+
+            if ($this->config['type'] === 'current') {
+                $name = 'Current load';
+                $load = $loads[0];
+                $description = "Load: " . $load . ".";
+            } else if ($this->config['type'] === 'average_5_min') {
+                $name = 'Load average 5 min';
+                $load = $loads[1];
+                $description = "Load average: " . $load . ".";
+            } else if ($this->config['type'] === 'average_15_min') {
+                $name = 'Load average 15 min';
+                $load = $loads[2];
+                $description = "Load average: " . $load . ".";
+            } else {
+                $name = $this->name;
                 $load = false;
-                $description = null;
-                $state = false;
-                $name = '';
-
-                if ($selected_load === 'current') {
-                    $load = $loads[0];
-                    $description = "Current load: $load.";
-                    $name = 'Current load';
-                } else if ($selected_load === 'average_5_min') {
-                    $load = $loads[1];
-                    $description = "Load average 5 min: $load.";
-                    $name = 'Load average 5 minutes';
-                } else if ($selected_load === 'average_15_min') {
-                    $load = $loads[2];
-                    $description = "Load average 15 min: $load.";
-                    $name = 'Load average 15 minutes';
-                } else {
-                    $load = false;
-                    $description = "Unsupported load (Only current, average_5_min and average_15_min are supported)";
-                    $name = $this->name . " ($selected_load)";
-                }
-
-                if ($load !== false) {
-                    if ($load > $error_threshold) {
-                       $state = ServerStates::error;
-                   } else if ($load > $warning_threshold) {
-                       $state = ServerStates::warning;
-                   } else {
-                       $state = ServerStates::ok;
-                   }
-                } else {
-                    $state = ServerStates::error;
-                    $load = false;
-                }
-
-                $result = new ServerHealthResult($name, $state, $description, $load);
-
-                $this->results[] = $result;
+                $description = "Unsupported load test (" . $this->config['type'] . ")";
             }
-        }
-	}
 
+            if ($load !== false) {
+                if ($load > $error_threshold) {
+                    $status = 2;
+                } else if ($load > $warning_threshold) {
+                    $status = 1;
+                } else {
+                    $status = 0;
+                }
+            } else {
+                $status = 2;
+            }
+
+            $result = new ServerHealthResult($name, $status, $description, $load);
+            $this->result = $result;
+        }
+    }
 }
