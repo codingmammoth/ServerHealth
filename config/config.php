b37ffd5e7aaa1cbc7315bf457e8793c6f0e57247
J.T. Kalkman <mail@jtkalkman.nl>
2023-08-11 13:37:05 +0100

    Adds method to get DB config.

--- config/config.php
+++ config/config.php
@@ -1,20 +1,11 @@
 <?php
 
-$error_level = E_ALL;
-
-$db_host  = ''; // Database-hostname (default: localhost)
-$db_user  = 'sammy'; // Database-username
-$db_pass  = 'password'; // Database-password
-$db_name  = 'example_database'; // Database name (database to select)
-$db_port  = 3306; // Default port number
-$db_tables = ['todo_list']; // Database table (to perform a little `select` on)
-
-$db_config = [
-    'db_host'  => $db_host, 
-    'db_user'  => $db_user, 
-    'db_user'  => $db_user, 
-    'db_pass'  => $db_pass, 
-    'db_name'  => $db_name, 
-    'db_port'  => $db_port,
-    'db_tables' => $db_tables
-];
+function getDBConfig() {
+    return [
+        'should_connect' => true,
+        'db_host' => '', // Database-hostname (default: localhost)
+        'db_user' =>'sammy', // Database-username
+        'db_pass' => 'password', // Database-password
+        'db_port' => 3306, // Default port number
+    ];
+}
