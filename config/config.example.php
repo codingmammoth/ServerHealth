2c15133cca3f849a91ca20dd50625c520999a2d7
JTKalkman <JTKalkman@users.noreply.github.com>
2023-08-17 12:28:59 +0200

    Improve configuration of tests in new server health library (#2)
    Adding more dynamic loading of custom tests, allowing easier updates

---
+++ config/config.example.php
@@ -0,0 +1,47 @@
+<?php
+
+function getConfig() {
+    $config = [
+        'db' => [
+            'connect' => true,
+            'db_host' => '', // Database-hostname (default: localhost)
+            'db_user' =>'sammy', // Database-username
+            'db_pass' => 'password', // Database-password
+            'db_port' => 3306, // Default port number
+        ],
+        'tests' => [
+            [
+                'test' => 'ServerLoad',
+                'config' => [ 'type' => 'current', 'warning_threshold' => 5, 'error_threshold' => 15 ]
+            ], 
+            [
+                'test' => 'ServerLoad',
+                'config' => [ 'type' => 'average_5_min', 'warning_threshold' => 5, 'error_threshold' => 15 ]
+        
+            ],
+            [
+                'test' => 'ServerLoad',
+                'config' => [ 'type' => 'average_15_min', 'warning_threshold' => 5, 'error_threshold' => 15 ]
+            ], 
+            [
+                'test' => 'MySQLPing',
+                'config' => []
+            ], 
+            [
+                'test' => 'MySQLFetchOperation',
+                'config' => [ 'database' => 'example_database', 'database_table' => 'todo_list' ]
+            ],
+            [
+                'test' => 'DiskSpace',
+                'config' => [ 
+                    'disks' => [
+                        ['name' => '/dev/sda1', 'warning_percentage_threshold' => 75, 'error_percentage_threshold' => 90],
+                        ['name' => '/dev/sda2', 'warning_percentage_threshold' => 75, 'error_percentage_threshold' => 90],
+                    ]
+                ]
+            ]
+        ]
+    ];
+
+    return $config;
+}
