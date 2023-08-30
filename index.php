1ab9b34de42b60b0c1cb0da3392cea2c55124685
JTKalkman <JTKalkman@users.noreply.github.com>
2023-08-30 13:47:51 +0200

    Sets status code 500 when not ok. (#4)

--- index.php
+++ index.php
@@ -4,6 +4,7 @@ require_once __DIR__."/config/config.php";
 
 require_once __DIR__."/src/ServerHealth/functions/functions.php";
 require_once __DIR__."/src/ServerHealth/ServerHealth.php";
+require_once __DIR__."/src/ServerHealth/ServerStates.php";
 
 error_reporting(0);
 
@@ -12,6 +13,8 @@ $tests = getTests($config);
 $health = new ServerHealth();
 $health->tests($tests);
 $results = $health->run();
+
+if ($results['status'] !== ServerStates::ok) { http_response_code(500); }
 
 header('Content-Type: application/json; charset=utf-8');
 echo json_encode($results);
